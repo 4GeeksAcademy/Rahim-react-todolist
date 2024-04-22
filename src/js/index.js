7752329cab998068af3af0e82e09124c2032ce0f
Rahim Afroz <108648650+afroz01@users.noreply.github.com>
2024-04-22 23:38:53 +0000

    components are imported

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+
 
 //import your own components
-import Home from "./component/home.jsx";
+import { EditTodoForm } from "../Components/EditTodoForm";
+import { Todo } from "../Components/Todo";
+import { TodoForm } from "../Components/TodoForm";
+import { TodoWrapper } from "../Components/TodoWrapper";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
